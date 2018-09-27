ef59d551318989f2df1d225d6ab254ad24635c36
Kalita Mikhail <mkalita1991@gmail.com>
2018-09-27 22:37:25 +0300

    more readable

--- src/index.js
+++ src/index.js
@@ -1,31 +1,29 @@
 module.exports = function solveEquation(equation) {
 	
-the_first_multiply_sing = equation.indexOf("*"); 
+  let firstMultiplySing = equation.indexOf("*");
+  let secondMultiplySing = equation.indexOf("*", firstMultiplySing + 1); 
+  
+  let a = equation.substring(0, firstMultiplySing - 1); 
+  let b = equation.substring(firstMultiplySing + 6, secondMultiplySing - 1); 
+  let c = equation.substring(secondMultiplySing + 4);
+  
+  b = b[0] + b.substring(2); 
+  c = c[0] + c.substring(2);
+  
+  let d = b * b - 4 * a * c; 
+  let solution1 = Math.round( ( - b + Math.sqrt(d) ) / ( 2 * a ) ); 
+  let solution2 = Math.round( ( - b - Math.sqrt(d) ) / ( 2 * a ) ); 
 
-the_second_multiply_sing = equation.indexOf("*", the_first_multiply_sing + 1); 
+  let array = []; 
 
-a=equation.substring(0,the_first_multiply_sing - 1); 
-b=equation.substring(the_first_multiply_sing + 6, the_second_multiply_sing - 1); 
-c=equation.substring(the_second_multiply_sing + 4); 
+  if ( solution2 > solution1 ) { 
+     array[0] = solution1;
+     array[1] = solution2; } 
+  
+  else { 
+      array[0] = solution2; 
+	  array[1] = solution1; } 
 
-
-b=b[0]+b.substring(2); 
-
-c=c[0]+c.substring(2); 
-
-var D = b * b - 4 * a * c; 
-
-
-solution1 = Math.round((- b + Math.sqrt(D)) / (2 * a)); 
-
-solution2 = Math.round((- b - Math.sqrt(D)) / (2 * a)); 
-
-var array = []; 
-
-if ( solution2 > solution1 ) { array[0] = solution1; array[1] = solution2; } 
-else { array[0] = solution2; array[1] = solution1; } 
-
-
-return array;
- 
+  return array;
+  
 }
